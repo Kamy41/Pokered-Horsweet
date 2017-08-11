4a1274e8d1ea8051ca13d8795ea4cec42e486d73
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2017-08-11 03:05:30 +0200

    Update trades.asm

--- data/trades.asm
+++ data/trades.asm
@@ -1,12 +1,12 @@
 TradeMons:
 ; givemonster, getmonster, textstring, nickname (11 bytes), 14 bytes total
-	db NIDORINO,  NIDORINA, 0,"NIDORINA@@@"
-	db ABRA,      MR_MIME,  0,"MR_MIME@@@@"
+	db NIDORINO,  NIDORINA, 0,"NIDOQUEEN@@"
+	db ABRA,      MR_MIME,  0,"MR.MIME@@@@"
 	db BUTTERFREE,BEEDRILL, 2,"BEEDRILL@@@"
-	db PONYTA,    SEEL,     0,"SEEL@@@@@@@"
-	db SPEAROW,   FARFETCHD,2,"FARFETCHD@@"
+	db PONYTA,    SEEL,     0,"DEWGONG@@@@"
+	db SPEAROW,   FARFETCHD,2,"FARFETCH'D@"
 	db SLOWBRO,   LICKITUNG,0,"LICKITUNG@@"
 	db POLIWHIRL, JYNX,     1,"JYNX@@@@@@@"
 	db RAICHU,    ELECTRODE,1,"ELECTRODE@@"
 	db VENONAT,   TANGELA,  2,"TANGELA@@@@"
-	db NIDORAN_M, NIDORAN_F,2,"NIDORAN_F@@"
+	db NIDORAN_M, NIDORAN_F,2,"NIDOQUEEN@@"

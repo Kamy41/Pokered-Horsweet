79eb8e460231293072f4c2b8840d82bea4f07c6d
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2018-03-19 02:33:54 +0100

    Update route9.asm

--- data/wildPokemon/route9.asm
+++ data/wildPokemon/route9.asm
@@ -4,12 +4,12 @@ Route9Mons:
 		db 16,RATTATA
 		db 16,SPEAROW
 		db 14,RATTATA
-		db 11,HORSYMEL
+		db 11,RATTATA
 		db 13,SPEAROW
 		db 15,EKANS
 		db 17,RATTATA
 		db 17,SPEAROW
-		db 13,HORSYMEL
+		db 13,EKANS
 		db 17,EKANS
 	ENDC
 	IF DEF(_BLUE)
